d0a6a65eee871b8c6708a7a51ad16513357effc4
kheteswar <kheteshwar.it@gmail.com>
2026-02-02 23:22:28 +0530

    Updated ConfigVisualizer.tsx

--- src/pages/ConfigVisualizer.tsx
+++ src/pages/ConfigVisualizer.tsx
@@ -39,6 +39,8 @@ import {
   ShieldOff,
   Layers,
   Route,
+  Cloud,
+  HardDrive,
 } from 'lucide-react';
 import { apiClient } from '../services/api';
 import { useApp } from '../context/AppContext';
