7c57a1cc43647ba786615f060b10a71d3ec4ff8b
kheteswar <kheteshwar.it@gmail.com>
2026-02-03 06:24:00 +0530

    Updated ConfigVisualizer.tsx

--- src/pages/ConfigVisualizer.tsx
+++ src/pages/ConfigVisualizer.tsx
@@ -47,7 +47,7 @@ import { useApp } from '../context/AppContext';
 import { useToast } from '../context/ToastContext';
 import type { Namespace, LoadBalancer, CDNLoadBalancer, CDNCacheRule, ParsedRoute, OriginPool, WAFPolicy, HealthCheck, ServicePolicy, ServicePolicyRule, AppType, AppSetting, AppTypeSetting, VirtualSite, UserIdentificationPolicy } from '../types';
 import { formatCertificateUrl, extractCertificateFromUrl } from '../utils/certParser';
-
+import { parseCertificateUrl } from '../utils/certParser';
 
 function isDefined<T>(v: T | null | undefined): v is T {
   return v !== undefined && v !== null;
